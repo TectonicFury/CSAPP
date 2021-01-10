a7254bc91189cedf091da65835912a1e2595a327
tf3 <11424232+TectonicFury@users.noreply.github.com>
2021-01-10 20:27:57 +0545

    added ex-2.60.c

---
+++ ch2/ex-2.60.c
@@ -0,0 +1,19 @@
+//ex-2.60.c
+
+#include <stdio.h>
+#include <limits.h>
+#include "show_bytes.h"
+unsigned replace_byte(unsigned x, int i, unsigned char b) {
+  char *p = (char*) &x;
+  p[i] = (p[i] & 0x00) | b;
+  return x;
+}
+
+int main() {
+  unsigned x = UINT_MAX;
+  unsigned y = replace_byte(x, 0, 0);
+  printf("%u\n", y);
+  show_unsigned(x);
+  show_unsigned(y);
+  return 0;
+}
--- ch2/show_bytes.c
+++ ch2/show_bytes.c
@@ -9,6 +9,9 @@ void show_bytes(byte_pointer start, size_t len) {
 void show_int(int x) {
   show_bytes((byte_pointer) &x, sizeof(int));
 }
+void show_unsigned(unsigned x) {
+  show_bytes((byte_pointer) &x, sizeof(unsigned));
+}
 void show_float(float x) {
   show_bytes((byte_pointer) &x, sizeof(float));
 }
