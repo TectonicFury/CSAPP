30d193f3bacc8d4115d415f70ee2a9c98cb65f2b
tf3 <11424232+TectonicFury@users.noreply.github.com>
2021-01-30 20:20:09 +0545

    ex-2.64

---
+++ ch2/ex-2.64.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int any_one_odd(unsigned x) {
+  unsigned y = 1;
+  int l = 2;
+
+  while (l < 32) {
+    y += (1 << l);
+    l += 2;
+  }
+  return (x & y) && 1;
+}
+
+int main(int argc, char const *argv[]) {
+  unsigned x = 32;
+  printf("%d\n", any_one_odd(x));
+  return 0;
+}
