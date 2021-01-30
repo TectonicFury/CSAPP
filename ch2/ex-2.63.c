12133b6f17cf298b994e77255c2cbf73d2450000
tf3 <11424232+TectonicFury@users.noreply.github.com>
2021-01-30 20:05:31 +0545

    ex-2.63.c

--- ch2/ex-2.63.c
+++ ch2/ex-2.63.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <limits.h>
 #include "show_bytes.h"
+
 unsigned srl(unsigned x, int k) {
   unsigned xsra = (int) x >> k;
-
   int l = 8*sizeof(int) - k - 1;
   unsigned y = 0;
   for ( ; l >= 0; l--) {
@@ -12,9 +13,19 @@ unsigned srl(unsigned x, int k) {
   return xsra;
 }
 
+unsigned sra(int x, int k) {
+  int xsrl = (unsigned) x >> k;
+  if (x > 0) return xsrl;
+  unsigned y = 0;
+  for (size_t i = 0; i <= k; i++) {
+    y += 1 << (31 - i);
+  }
+  return xsrl | y;
+}
+
 int main(int argc, char const *argv[]) {
-  unsigned v = 12345678;
-  unsigned y = srl(v, 16);
+  int v = 16;
+  unsigned y = sra(v, 4);
   show_unsigned(v);
   printf("%u\n", y);
   show_unsigned(y);
